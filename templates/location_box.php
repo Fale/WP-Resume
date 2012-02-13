c5f88f1e8051b729dce2ef54b26a2aac303e1fe3
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-13 21:12:54 +0100

    Add the template/location_box.php file, using template/date_box.php as guide

---
+++ templates/location_box.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * Template for location box inputs
+ * @package WP_Resume
+ */
+?><label for="location"><?php _e( 'Location', 'wp-resume' ); ?>
+	<input type="text" name="location" id="location" value="<?php echo $from; ?>" placeholder="Write it if you want to override the Organization one"/>
+</label>
